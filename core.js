c28f5dc02d0f0ed5ecf2680689f587b9def282f9
Vazer Rahmani <vaz3r@outlook.com>
2018-08-10 19:29:50 +0100

    v1.0

---
+++ core.js
@@ -0,0 +1,9 @@
+/* Open when someone clicks on the span element */
+function openNav() {
+    document.getElementById("mobile-nav").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+    document.getElementById("mobile-nav").style.width = "0%";
+}
