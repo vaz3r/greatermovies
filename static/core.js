ec0ec4a56dbde532b0f9b093e06b60cfcc77a2a8
Vazer Rahmani <vaz3r@outlook.com>
2018-08-26 03:15:00 +0100

    v2.4

--- static/core.js
+++ static/core.js
@@ -5,29 +5,3 @@ function openNav() {
 function closeNav() {
     document.getElementById("mobile-nav").style.width = "0%";
 }
-
-var disqus_config = function () {
-    this.page.url = "https://vaz3r.github.io/greatermovies/";
-    this.page.identifier = "page-netflix-best-right-now";
-};
-
-function loadDisqus() {
-    var d = document,
-        s = d.createElement('script');
-    s.src = 'https://greater-movies.disqus.com/embed.js';
-    s.setAttribute('data-timestamp', +new Date());
-    s.async = true;
-    (d.head || d.body).appendChild(s);
-}
-
-if (document.readyState === 'ready' || document.readyState === 'complete') {
-    loadDisqus();
-    console.log("completed");
-} else {
-    document.onreadystatechange = function () {
-        if (document.readyState === 'ready' || document.readyState === 'complete') {
-            loadDisqus();
-            console.log("completed");
-        }
-    }
-}
