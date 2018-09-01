f4120899af735d08d11b04d0b799f3567632d580
Vazer Rahmani <vaz3r@outlook.com>
2018-09-01 02:46:40 +0100

    v3.5

--- static/core.js
+++ static/core.js
@@ -5,3 +5,19 @@ function openNav() {
 function closeNav() {
     document.getElementById("mobile-nav").style.width = "0%";
 }
+
+// VIDEO MODAL
+var vModal = document.getElementById("vModal");
+var youtubeVideo = document.getElementById("youtubeVideo");
+
+window.onclick = function(event) {
+    if (event.target == vModal) {
+        youtubeVideo.setAttribute("src", "");
+        vModal.style.display = "none";
+    }
+};
+
+function playVideo(videoID) {
+    vModal.style.display = "flex";
+    youtubeVideo.setAttribute("src", "https://www.youtube.com/embed/" + videoID + "?rel=0&amp;showinfo=0");
+}
