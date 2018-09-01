533f07660756fd8b19b57553badb8feb77eb5824
Vazer Rahmani <vaz3r@outlook.com>
2018-09-01 19:44:35 +0100

    v4

--- static/core.js
+++ static/core.js
@@ -26,62 +26,6 @@ function playVideo(videoID) {
 var trailersList = document.getElementById("trailersList");
 var dataID = trailersList.getAttribute("dataType");
 
-function fetchData(database) {
-    fetch('https://greatermovies.com/trailers/data/' + database + '.json')
-        .then(
-            function (response) {
-                if (response.status !== 200) {
-                    console.log('Looks like there was a problem. Status Code: ' +
-                        response.status);
-                    return;
-                }
-
-                response.json().then(function (data) {
-                    data.trailers.forEach(function (obj) {
-                        if (obj.image.includes("http")) {
-                            if (!trailersList.innerHTML.includes(obj.title)) {
-                                var trailerCard = document.createElement("article");
-                                trailerCard.className = "trailer-card";
-                                trailerCard.onclick = function () {
-                                    playVideo(obj.video_url.replace("https://www.youtube.com/watch?v=", ""));
-                                };
-
-                                var cardTrailer = document.createElement("article");
-                                cardTrailer.className = "card-trailer";
-
-                                var cardPlay = document.createElement("img");
-                                cardPlay.className = "trailer-play";
-                                cardPlay.src = "https://greatermovies.com/static/play.png";
-                                cardPlay.alt = "play";
-
-                                var cardPoster = document.createElement("img");
-                                cardPoster.title = obj.title;
-                                cardPoster.alt = obj.title;
-                                cardPoster.src = obj.image;
-
-                                var cardTitle = document.createElement("h2");
-                                cardTitle.innerText = obj.title;
-
-                                cardTrailer.appendChild(cardPlay);
-                                cardTrailer.appendChild(cardPoster);
-                                cardTrailer.appendChild(cardTitle);
-
-                                trailerCard.appendChild(cardTrailer);
-
-                                trailersList.appendChild(trailerCard);
-                            }
-                        } else {
-                            console.log("NO_IMG");
-                        }
-                    });
-                });
-            }
-        )
-        .catch(function (err) {
-            console.log('Fetch Error :-S', err);
-        });
-}
-
 function getData(database) {
     var xhttp = new XMLHttpRequest();
 
@@ -124,8 +68,6 @@ function getData(database) {
                     }
                 }
             });
-        } else {
-            console.log(this.status);
         }
     };
 
