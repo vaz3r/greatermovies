b3b4dc3ec73bf5461b59ff7d78a758efc3e3c7f5
Vazer Rahmani <vaz3r@outlook.com>
2018-09-01 19:07:17 +0100

    v4

--- static/core.js
+++ static/core.js
@@ -87,8 +87,45 @@ function getData(database) {
 
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
-            var obj = JSON.parse(xhttp.responseText);
-            console.log(obj.trailers[0].title);
+            var data = JSON.parse(xhttp.responseText);
+
+            data.trailers.forEach(function (obj) {
+                if (obj.image.includes("http")) {
+                    if (!trailersList.innerHTML.includes(obj.title)) {
+                        var trailerCard = document.createElement("article");
+                        trailerCard.className = "trailer-card";
+                        trailerCard.onclick = function () {
+                            playVideo(obj.video_url.replace("https://www.youtube.com/watch?v=", ""));
+                        };
+
+                        var cardTrailer = document.createElement("article");
+                        cardTrailer.className = "card-trailer";
+
+                        var cardPlay = document.createElement("img");
+                        cardPlay.className = "trailer-play";
+                        cardPlay.src = "https://greatermovies.com/static/play.png";
+                        cardPlay.alt = "play";
+
+                        var cardPoster = document.createElement("img");
+                        cardPoster.title = obj.title;
+                        cardPoster.alt = obj.title;
+                        cardPoster.src = obj.image;
+
+                        var cardTitle = document.createElement("h2");
+                        cardTitle.innerText = obj.title;
+
+                        cardTrailer.appendChild(cardPlay);
+                        cardTrailer.appendChild(cardPoster);
+                        cardTrailer.appendChild(cardTitle);
+
+                        trailerCard.appendChild(cardTrailer);
+
+                        trailersList.appendChild(trailerCard);
+                    }
+                } else {
+                    console.log("NO_IMG");
+                }
+            });
         }
     };
 
