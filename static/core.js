ff7575bd2d89e167f6b57985e5513cc20b340bda
Vazer Rahmani <vaz3r@outlook.com>
2018-09-02 22:18:00 +0100

    v4.1

--- static/core.js
+++ static/core.js
@@ -22,9 +22,55 @@ function playVideo(videoID) {
     youtubeVideo.setAttribute("src", "https://www.youtube.com/embed/" + videoID + "?rel=0&amp;showinfo=0&autoplay=1");
 }
 
+// MOVIES ===================================================================================
+
+function fetchMovies() {
+    var cards = document.getElementById("cards");
+
+    if (cards !== null) {
+        var database = cards.getAttribute("data-uri");
+
+        if (database !== undefined) {
+            var httpClient = new XMLHttpRequest();
+
+            httpClient.onreadystatechange = function () {
+                if (this.readyState == 4 && this.status == 200) {
+                    var json = JSON.parse(httpClient.responseText);
+                    var index = 1;
+                    json.data.forEach(function (obj) {
+                        if (!cards.innerHTML.includes(obj.title)) {
+                            var card_template = '<article class="card"> <figure class="card-left"> <img alt="{#TITLE}" src="{#IMAGE}"/> </figure> <article class="card-right"> <h2> <span>#{#INDEX}</span>{#TITLE}</h2> <div class="card-ratings"> <div class="rating"> <span title="IMDb" class="imdb">IMDb</span> <span class="card-score">{#RATING}</span> </div><div class="rating"> <span title="Runtime" class="runtime"> <span class="icon-stopwatch"></span> </span> <span class="card-score">{#TIME}</span> </div></div><article title="{#TITLE}Summary" class="fan-review"> <p>{#SUMMARY}</p></article> <article class="buttons"> <a> <div class="button-trailer" onclick="playVideo(\'{#TRAILER}\');"> <span class="icon-youtube"></span> WATCH TRAILER </div></a> </article> </article> </article>';
+
+                            card_template = card_template.replace("{#TITLE}", obj.title);
+                            card_template = card_template.replace("{#IMAGE}", obj.image);
+                            card_template = card_template.replace("{#INDEX}", index);
+                            card_template = card_template.replace("{#RATING}", obj.rating);
+                            card_template = card_template.replace("{#TIME}", obj.time);
+                            card_template = card_template.replace("{#SUMMARY}", obj.summary);
+                            card_template = card_template.replace("{#TRAILER}", obj.video_url.replace("https://www.youtube.com/watch?v=", ""));
+
+                            cards.innerHTML += card_template;
+
+                            index += 1;
+                        }
+                    });
+                }
+            };
+
+            httpClient.open("GET", 'https://greatermovies.com/movies/data/' + database + '/database.json', true);
+            httpClient.send();
+        }
+    }
+}
+
+fetchMovies();
+
 // TRAILERS ===================================================================================
 var trailersList = document.getElementById("trailersList");
-var dataID = trailersList.getAttribute("dataType");
+
+if (trailersList !== null) {
+    var database = trailersList.getAttribute("dataType");
+}
 
 function getData(database) {
     var xhttp = new XMLHttpRequest();
@@ -75,8 +121,8 @@ function getData(database) {
     xhttp.send();
 }
 
-if (dataID !== "") {
-    getData(dataID);
+if (database !== undefined) {
+    getData(database);
 }
 
 // Notifications ==========================================================================
