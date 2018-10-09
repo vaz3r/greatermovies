b2fb1d30d5ce1835fde988f5134c8512157c6996
Vazer Rahmani <vaz3r@outlook.com>
2018-10-09 01:56:01 +0100

    v59

--- static/core.js
+++ static/core.js
@@ -64,7 +64,6 @@ function fetchDatabase() {
                         if (index == adCount) {
                             if (index <= 40) {
                                 cards.innerHTML += '<ins class="adsbygoogle" style="display:block" data-ad-format="fluid" data-ad-layout-key="-9y+et+3f-up+17f" data-ad-client="ca-pub-7903023966618400" data-ad-slot="9915892709"></ins><script>(adsbygoogle = window.adsbygoogle || []).push({});</script>';
-                                cards.innerHTML += '<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-7903023966618400" data-ad-slot="2349104311" data-ad-format="auto" data-full-width-responsive="true"></ins><script>(adsbygoogle = window.adsbygoogle || []).push({});</script>';
                                 adCount += 5;
                                 (adsbygoogle = window.adsbygoogle || []).push({});
                             }
